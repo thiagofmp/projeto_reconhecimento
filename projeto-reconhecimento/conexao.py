877351dcae92d98914d758cb1b8c5904eda60963
thiagofmp <160027666+thiagofmp@users.noreply.github.com>
2026-06-06 18:35:24 -0300

    Delete projeto-reconhecimento directory

--- projeto-reconhecimento/conexao.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import os
-
-def get_conexao():
-    # Troque pela URI do seu MongoDB Atlas depois
-    MONGO_URI = os.getenv("MONGO_URI")
-    client = MongoClient(MONGO_URI)
-    return client['reconhecimento_facial']
